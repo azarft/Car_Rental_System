20f39abb346b70f585d733095f55cd90156f41e1
azarft <126442414+azarft@users.noreply.github.com>
2023-11-22 14:06:18 +0600

    Create project

---
+++ src/main/java/com/example/car_rental_system_final/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.car_rental_system_final;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.car_rental_system_final {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.car_rental_system_final to javafx.fxml;
+    exports com.example.car_rental_system_final;
+}
