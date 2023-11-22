3e140b23279e1daa4a3f3e11fbe7b3d6644b3770
azarft <126442414+azarft@users.noreply.github.com>
2023-11-22 16:10:25 +0600

    Add SignUp page

---
+++ src/main/java/com/example/car_rental_system_final/Car_Rental_System.java
@@ -0,0 +1,33 @@
+package com.example.car_rental_system_final;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Car_Rental_System extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main_page.fxml"));
+        Parent root = fxmlLoader.load();
+
+        // Set the main page as the root of the scene
+        Scene scene = new Scene(root, 900, 600);
+
+        // Set the controller and the primary stage
+        Car_Rental_Controller controller = fxmlLoader.getController();
+        controller.setPrimaryStage(stage);
+
+        stage.setTitle("Car Rental System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/car_rental_system_final/Sign_up_Controller.java
@@ -0,0 +1,9 @@
+package com.example.car_rental_system_final;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Sign_up_Controller {
+    @FXML
+    private Button click;
+}
